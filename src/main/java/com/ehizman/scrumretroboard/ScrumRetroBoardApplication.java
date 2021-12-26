e42078ed273338d79248a785c07e73e2fb805e81
Ehis <edemaehiz@gmail.com>
2021-12-26 02:10:08 +0100

    added project dependencies

---
+++ src/main/java/com/ehizman/scrumretroboard/ScrumRetroBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ehizman.scrumretroboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumRetroBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrumRetroBoardApplication.class, args);
+    }
+
+}
